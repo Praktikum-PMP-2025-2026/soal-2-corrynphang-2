efefe5b509234552b1c06b0c306455406d7d29bc
Beatrice Corryn Pangestu <124460876+corrynphang@users.noreply.github.com>
2026-05-11 10:03:41 +0700

    TESTING 3

--- soal2.c
+++ soal2.c
@@ -11,90 +11,93 @@ misi dijalankan.
 Tentukan apakah semua misi dapat diselesaikan jika tiap misi memiliki prasyarat.
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
 
- #include <stdio.h>
- #include <stdlib.h>
+#define MAX_VERTICES 100
 
- typedef struct node{
-    int dest;
-    struct node*next;
- }node;
-
- typedef struct graf{
+struct Graph {
     int v;
-    struct node** array;
- }graf;
+    int** array;
+};
 
- node* new(int dest){
-    node* new =(node*)malloc(sizeof(node));
-    new ->dest = dest;
-    new ->next = NULL;
-    return new;
- }
+struct Graph* create(int vertices)
+{
+    struct Graph* graph= (struct Graph*)malloc(sizeof(struct Graph));
+    graph->v = vertices;
 
- graf* create(int v){
-    graf* graph=(graf*) malloc(sizeof(graf));
-    graph -> v = v;
-    graph -> array=calloc (v,sizeof(node*));
+    graph->array= (int**)malloc(vertices * sizeof(int*));
+    for (int i = 0; i < vertices; i++) {
+        graph->array[i] = (int*)malloc(vertices * sizeof(int));
+        for (int j = 0; j < vertices; j++) {
+            graph->array[i][j] = 0;
+        }
+    }
     return graph;
- }
+}
 
- void edge(graf* graph, int src, int dest){
-    node* node = new(dest);
-    node->next= graph->array[src];
-    graph->array[src]= node;
-    node = new(src);
-    node -> next = graph-> array[dest];
-    graph -> array[dest]=node;
- }
+void edge(struct Graph* graph, int src, int dest)
+{
+    graph->array[src][dest] = 1;
+}
 
- int dfs(graf* graph,int v, int* visit, int*stack){
-    if(!visit[v]){
-        visit[v]=1;
-        stack[v]=1;
-        for (int i =0; i < graph->v;i++){
-            if (graph->array[v]&& graph->array[i]){
-                if(!visit[i] && dfs(graph,v,visit,stack)){
+int dfs(struct Graph* graph, int vertex,
+                      int* visited, int* stack)
+{
+    if (!visited[vertex]) {
+        visited[vertex] = 1;
+        stack[vertex] = 1;
+        for (int v = 0; v < graph->v; v++) {
+            if (graph->array[vertex][v]) {
+                if (!visited[v]&& dfs(graph, v, visited,stack)) {
                     return 1;
-                }else if (stack[i]){
+                }
+                else if (stack[v]) {
                     return 1;
                 }
             }
         }
     }
-    stack[v]=0;
+    stack[vertex]= 0; 
     return 0;
- }
+}
+int detect(struct Graph* graph)
+{
+    int* visited = (int*)malloc(graph->v * sizeof(int));
+    int* stack = (int*)malloc(graph->v * sizeof(int));
 
- int detect(graf* graph){
-    int*visit= (int*) malloc(graph->v* sizeof(int));
-    int* stack=(int*) malloc(graph->v* sizeof(int));
-    for (int i=0;i<graph->v;i++){
-        visit[i]=0;
-        stack[i]=0;
+    for (int i = 0; i < graph->v; i++) {
+        visited[i] = 0;
+        stack[i] = 0;
     }
-    for (int i=0; i< graph->v;i++){
-        if(dfs(graph,i,visit,stack)){
+
+    for (int i = 0; i < graph->v; i++) {
+        if (dfs(graph, i, visited,
+                              stack)) {
             return 1;
         }
     }
     return 0;
- }
- int main(){
-    int V;
+}
+
+int main()
+{
+     int V;
     int M;
     int a;
     int b;
     scanf("%d %d ",&V,&M);
-    graf* graph = create(V);
+    struct Graph* graph = create(V);
     for (int i=0; i<M; i++){
-        scanf("%d %d",&a,&b);
-        edge(graph, a, b);
-    }
-    if (detect(graph)){
+            scanf("%d %d",&a,&b);
+            edge(graph, a, b);
+        }   
+    if (detect(graph)) {
         printf("TIDAK BISA");
-    }else{
+    }
+    else {
         printf("BISA");
     }
+
     return 0;
- }
+}
